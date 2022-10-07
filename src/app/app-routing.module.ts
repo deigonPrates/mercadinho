2d5647d09cefc9feed2ccfdfb1d04278d50d0a58
Deigon Prates <deigon_prates@hotmail.com>
2022-10-07 20:55:41 -0300

    iniciando o app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./component/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'vendas',
+    loadChildren: () => import('./component/vendas/vendas.module').then((m) => m.VendasModule),
+  },
+  {
+    path: 'produtos',
+    loadChildren: () => import('./component/produtos/produtos.module').then((m) => m.ProdutosModule),
+  },
+  {
+    path: 'tipos',
+    loadChildren: () => import('./component/tipos/tipos.module').then((m) => m.TiposModule),
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
